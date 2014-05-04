d3aa4e191baf13e8371c0f1d300cfe0ee7bb2d89
jmontuiuc <j-monton@kellogg.northwestern.edu>
2014-05-04 14:36:21 -0500

    Defined @movie to display Move Info based on URL

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
     # Your code goes here
     # Use the information after the slash in the URL to look up the movie with the corresponding ID number
     # Store the movie in the @movie instance variable so the view can format it
-
-    # @movie = ???
+    movieid = params[:id]
+    movieinfo = Movie.find_by({:id => movieid})
+    @movie = movieinfo
   end
 end
